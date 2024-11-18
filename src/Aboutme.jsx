292315b34c1426f78e75422a7b8b7fb3fa65d72f
FelixNihBous <felixlix604@gmail.com>
2024-11-18 11:40:35 +0700

    Newest

--- src/Aboutme.jsx
+++ src/Aboutme.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 import './css/Aboutme.css'
 import Header from './Header.jsx'
 import dogImage from './assets/dog.jpg/';
@@ -8,11 +9,17 @@ import dogImage from './assets/dog.jpg/';
 function Aboutme() {
     return (
         <>
-            <Header />
+            <motion.div className="aboutme"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 1, duration: 3 }}
+            >
+                <Header />
+                <div className='container'>
+                    <img src={dogImage} className='dogimage'></img>
+                </div>
+            </motion.div>
 
-            <div className='container'>
-                <img src={dogImage} className='dogimage'></img>
-            </div>
         </>
     );
 }
--- src/Newpage.jsx
+++ src/Newpage.jsx
@@ -1,97 +1,63 @@
 import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 import dogImage from './assets/dog.jpg/';
 import Header from './Header.jsx';
-import './css/Newpage.css'
+import './css/Newpage.css';
 
-function Newpage({ isOpen, onClose, children }) {
+const stories = [
+    { id: 1, title: 'Story 1 Title', content: 'This is the content of story 1' },
+    { id: 2, title: 'Story 2 Title', content: 'This is the content of story 2' },
+    { id: 1, title: 'Story 1 Title', content: 'This is the content of story 1' },
+    { id: 2, title: 'Story 2 Title', content: 'This is the content of story 2' }, { id: 1, title: 'Story 1 Title', content: 'This is the content of story 1' },
+    { id: 2, title: 'Story 2 Title', content: 'This is the content of story 2' }, { id: 1, title: 'Story 1 Title', content: 'This is the content of story 1' },
+    { id: 2, title: 'Story 2 Title', content: 'This is the content of story 2' }, { id: 1, title: 'Story 1 Title', content: 'This is the content of story 1' },
+    { id: 2, title: 'Story 2 Title', content: 'This is the content of story 2' },];
+
+function Newpage() {
+    const [selectedStoryId, setSelectedStoryId] = useState(null);
+
+    const handleStoryClick = (storyId) => {
+        setSelectedStoryId(storyId);
+    };
+
+    const closePopup = () => {
+        setSelectedStoryId(null);
+    };
+
     return (
         <>
+            <motion.div className='content-story'
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 1, duration: 3 }}
+            >
+                <Header />
+                <div className="mainbody">
+                    {stories.map((story) => (
+                        <div className="gridcontainer" key={story.id}>
+                            <a href="#" onClick={() => handleStoryClick(story.id)}>
+                                <div className="imagegrid">
+                                    <img src={dogImage} alt="Placeholder image" className="" />
+                                </div>
+                                <div className="textgrid">{story.title}</div>
+                            </a>
+                        </div>
+                    ))}
+                </div>
 
-            <Header />
-            <div className='mainbody'>
-                <div className='gridcontainer'>
-                    <a href='https://google.com/'>
-                        <div className='imagegrid'>
-                            <img src={dogImage} className=''></img>
-                        </div>
-                        <div className='textgrid'>asdasdasd</div>
-                    </a>
-                </div>
-                <div className='gridcontainer'>
-                    <div className='imagegrid'></div>
-                    <div className='textgrid'>asdasdasd</div>
-                </div>
-                <div className='gridcontainer'>
-                    <div className='imagegrid'></div>
-                    <div className='textgrid'>asdasdasd</div>
-                </div>
-                <div className='gridcontainer'>
-                    <a href='https://google.com/'>
-                        <div className='imagegrid'>
-                            <img src={dogImage} className=''></img>
-                        </div>
-                        <div className='textgrid'>asdasdasd</div>
-                    </a>
-                </div>
-                <div className='gridcontainer'>
-                    <div className='imagegrid'></div>
-                    <div className='textgrid'>asdasdasd</div>
-                </div>
-                <div className='gridcontainer'>
-                    <div className='imagegrid'></div>
-                    <div className='textgrid'>asdasdasd</div>
-                </div>
-                <div className='gridcontainer'>
-                    <a href='https://google.com/'>
-                        <div className='imagegrid'>
-                            <img src={dogImage} className=''></img>
-                        </div>
-                        <div className='textgrid'>asdasdasd</div>
-                    </a>
-                </div>
-                <div className='gridcontainer'>
-                    <div className='imagegrid'></div>
-                    <div className='textgrid'>asdasdasd</div>
-                </div>
-                <div className='gridcontainer'>
-                    <div className='imagegrid'></div>
-                    <div className='textgrid'>asdasdasd</div>
-                </div>
-                <div className='gridcontainer'>
-                    <a href='https://google.com/'>
-                        <div className='imagegrid'>
-                            <img src={dogImage} className=''></img>
-                        </div>
-                        <div className='textgrid'>asdasdasd</div>
-                    </a>
-                </div>
-                <div className='gridcontainer'>
-                    <div className='imagegrid'></div>
-                    <div className='textgrid'>asdasdasd</div>
-                </div>
-                <div className='gridcontainer'>
-                    <div className='imagegrid'></div>
-                    <div className='textgrid'>asdasdasd</div>
-                </div>
-                <div className='gridcontainer'>
-                    <a href='https://google.com/'>
-                        <div className='imagegrid'>
-                            <img src={dogImage} className=''></img>
-                        </div>
-                        <div className='textgrid'>asdasdasd</div>
-                    </a>
-                </div>
-                <div className='gridcontainer'>
-                    <div className='imagegrid'></div>
-                    <div className='textgrid'>asdasdasd</div>
-                </div>
-                <div className='gridcontainer'>
-                    <div className='imagegrid'></div>
-                    <div className='textgrid'>asdasdasd</div>
-                </div>
-            </div>
+                {selectedStoryId && (
+                    <div className="story-popup">
+                        <button onClick={closePopup}>&times;</button>
+                        <h2>{stories.find((story) => story.id === selectedStoryId)?.title}</h2>
+                        <p>{stories.find((story) => story.id === selectedStoryId)?.content}</p>
+                    </div>
+                )}
 
+                {selectedStoryId && ( // Apply blur effect when popup is open
+                    <div className="popup-overlay" onClick={closePopup}></div>
+                )}
+            </motion.div>
         </>
     );
 }
