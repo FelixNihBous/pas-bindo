fc09264055439fdca3e551c6f27a370336fceb00
FelixNihBous <felixlix604@gmail.com>
2024-11-18 09:16:28 +0700

    Fix 2

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Routes basename={process.env.PUBLIC_URL}> 
+        <Routes> 
           <Route path="/" element={<Homepage />} />
           <Route path="/newpage"
             element={<Newpage />} />
